11fd63ec69cf744f19430c0b5eb6824868bf5fe3
symi <symi.contact@gmail.com>
2025-04-10 20:00:10 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const data = ref(null)
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://localhost:5001/api/example')
+    data.value = response.data
+  } catch (err) {
+    console.error(err)
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>API Data:</h1>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
